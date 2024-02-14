468cc0bc6c583e8a58a044ffda5dcbc217ac560c
Tim Birger Tejsner <tbirger@ruc.dk>
2024-02-14 13:49:40 +0100

    Generalize integrate() so we can use it with GISAXS

--- src/rucsaxslib/hist.py
+++ src/rucsaxslib/hist.py
@@ -4,35 +4,28 @@ Azimuthal integration of images
 import numpy as np
 
 
-def integrate(img, coords='q', bins=500, xrange=None):
-    mask = ~np.isnan(img.data)
+def integrate(img, x=None, mask=None, **kwargs):
+    if mask is None:
+        mask = ~np.isnan(img.data)
 
-    if coords == 'q':
+    if x is None or x == 'q':
         x = img.get_q()
-    elif coords == 'tth':
-        x, _ = img.get_coordinates(reference_system='polar')
 
-    y_total, x_edges = np.histogram(x[mask],
-                                    bins=bins,
-                                    range=xrange,
-                                    weights=img.data[mask])
+    default_kwargs = {'bins': 500}
+    if kwargs:
+        kwargs = {**default_kwargs, **kwargs}
+    else:
+        kwargs = default_kwargs
 
-    y_npix, _ = np.histogram(x[mask],
-                             range=xrange,
-                             bins=bins)
-
+    y_total, x_edges = np.histogram(x[mask], weights=img.data[mask], **kwargs)
+    y_npix, _ = np.histogram(x[mask], **kwargs)
     y_total[y_total == 0] = np.nan
     y = y_total/y_npix
 
-    # error
-    variance, _ = np.histogram(x[mask],
-                               bins=bins,
-                               range=xrange,
-                               weights=img.error[mask]**2)
+    variance, _ = np.histogram(x[mask], weights=img.error[mask]**2, **kwargs)
     error_total = np.sqrt(variance)
     error = error_total/y_npix
 
-    # get x values (bin centers)
     x = x_edges[:-1] + np.diff(x_edges)/2
 
     return x, y, error
