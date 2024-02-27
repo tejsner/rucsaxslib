0287ea0d3747d2428d86a546e7ffec09100db819
Tim Birger Tejsner <tbirger@ruc.dk>
2024-02-27 12:59:28 +0100

    stva helper functions initial commit

--- src/rucsaxslib/__init__.py
+++ src/rucsaxslib/__init__.py
@@ -4,6 +4,7 @@ rucsaxslib docstring
 from .img import ImgData, from_file, from_rucsaxs
 from .hist import integrate
 from .gisaxs import gi_integrate, gi_draw_bbox, reflectivity
+from . import stva
 
 __all__ = ['ImgData', 'from_file', 'from_rucsaxs', 'integrate',
-           'gi_integrate', 'gi_draw_bbox', 'reflectivity']
+           'gi_integrate', 'gi_draw_bbox', 'reflectivity', 'stva']
